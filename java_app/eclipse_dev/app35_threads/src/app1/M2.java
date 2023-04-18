d1d699c90f20440042a70da6cf84791cd840c747
Dev <divyanshu.sunnykumar1999@gmail.com>
2023-04-18 17:54:10 +0530

    adding thread notes 18/4/23
    -Thread introduction

---
+++ java_app/eclipse_dev/app35_threads/src/app1/M2.java
@@ -0,0 +1,17 @@
+package app1;
+class A extends Thread{	//inbuilt class
+	public void run() {	//inherited method overriding
+		for (int i = 1; i < 1000 ; i++) {
+			System.out.println("loop1: " + i);
+		}
+	}
+}
+public class M2 {
+	public static void main(String[] args) {
+		A a1 = new A();
+		a1.start();
+		for (int i = 4000; i < 5000; i++) {
+			System.out.println("loop2: " + i);
+		}
+	}
+}
---
+++ java_app/eclipse_dev/app35_threads/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Dev
+ *
+ */
+module app35_threads {
+}
