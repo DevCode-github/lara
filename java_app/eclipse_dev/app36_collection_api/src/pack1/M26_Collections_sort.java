a9d5f6f87975b347e744f6135b255d4f1f9440a8
Dev <divyanshu.sunnykumar1999@gmail.com>
2023-04-22 21:16:00 +0530

    Renaming files with meaningful names

---
+++ java_app/eclipse_dev/app36_collection_api/src/pack1/M26_Collections_sort.java
@@ -0,0 +1,20 @@
+package pack1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+/* Integer reversal */
+public class M26_Collections_sort {
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add(100);
+		list.add(10);
+		list.add(200.0);//All element should be of same typeN
+		list.add(0);
+		list.add(400);
+		list.add(50);
+		System.out.println(list);
+		Collections.sort(list);
+		Collections.sort(list,Collections.reverseOrder());
+		System.out.println(list);
+	}
+}
