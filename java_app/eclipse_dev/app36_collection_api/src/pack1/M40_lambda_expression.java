6e726d1e117755fbd1abcc52d42933892959c925
Dev <divyanshu.sunnykumar1999@gmail.com>
2023-04-26 09:35:15 +0530

    updating java class notes and info.txt - 26/4/23 M40-M41
    -lambda expression

---
+++ java_app/eclipse_dev/app36_collection_api/src/pack1/M40_lambda_expression.java
@@ -0,0 +1,18 @@
+package pack1;
+interface N{
+	void test();
+}
+public class M40_lambda_expression {
+	public static void main(String[] args) {
+		@SuppressWarnings({"unchecked","rawtypes"})
+		N n1 = new N() {
+			@Override
+			public void test() {
+				System.out.println("g-test lambda");				
+			}
+		};
+		N n2 = () -> System.out.println("g-test lambda");
+		n1.test();
+		n2.test();
+	}
+}
---
+++ java_app/eclipse_dev/app36_collection_api/src/pack1/M42_lambda_expression.java
@@ -0,0 +1,16 @@
+package pack1;
+interface P{
+	void test(int i);
+//	void test2(int k,int i);
+}
+public class M42_lambda_expression {
+	@SuppressWarnings({ "unused" })
+	public static void main(String[] args) {
+		P obj = (int i) -> System.out.println("obj-test");
+		P obj1 = (i) -> System.out.println("obj1-test");
+		P obj2 = (k) -> System.out.println("obj2-test");
+		obj.test(0);
+		obj1.test(10);
+		obj2.test(20);
+	}
+}
