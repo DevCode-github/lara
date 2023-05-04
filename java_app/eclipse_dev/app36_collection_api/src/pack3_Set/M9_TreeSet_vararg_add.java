53a4c7491caa9c54a6b5be2f60944070ccc56a8e
Dev <divyanshu.sunnykumar1999@gmail.com>
2023-05-04 12:03:47 +0530

    Making Changes in C class and comparator.
    -class C(int i, int j) -> class C(int i, int j, int k)
    -making Comparator use hashCode() and equal() method to check for
     equality and comparison together.
    -adding description for the comparator.

--- java_app/eclipse_dev/app36_collection_api/src/pack3_Set/M9_TreeSet_vararg_add.java
+++ java_app/eclipse_dev/app36_collection_api/src/pack3_Set/M9_TreeSet_vararg_add.java
@@ -3,21 +3,21 @@ package pack3_Set;
 import java.util.Comparator;
 import java.util.TreeSet;
 class C{
-	int i,j;
-	C(int i,int j) {
+	int i,j,k;
+	C(int i,int j, int k) {
 		this.i = i;
 		this.j = j;
+		this.k = k;
 	}
 	public String toString() {
-		return "("+i+","+j+")";
+		return "("+i+","+j+","+k+")";
 	}
 	public int hashCode() {
-		return Integer.toString(i).hashCode() + Integer.toString(j).hashCode();
-    }
-	
+		return Integer.toString(i).hashCode() - Integer.toString(j).hashCode() - Integer.toString(k).hashCode();
+    }	
 	public boolean equals(Object o)
 	{
-		return this.i == ((C)o).i && this.j == ((C)o).j;
+		return o instanceof C && this.i == ((C)o).i && this.j == ((C)o).j && this.k == ((C)o).k;
 	}
 }
 @SuppressWarnings({"unchecked","rawtypes","serial"})
@@ -49,11 +49,41 @@ class TreeSet1 extends TreeSet{
 }
 public class M9_TreeSet_vararg_add {
 	public static void main(String[] args) {
-		TreeSet1 set = new TreeSet1((o1,o2) -> ((C)o1).i - ((C)o2).i);
-		System.out.println(set.addMany(new C(2,3),new C(4,5),new C(5,6)));
-		System.out.println(set.addMany(new C(2,3),new C(4,5),new C(5,6)));
-		System.out.println(set.addMany(new C(2,3),new C(4,5),new C(5,6)));
+		/*
+		 * Tree set is not using hashCode() and equal() method.
+		 * Tree set if discarding object if compare method determines its field which we specified, as equal,
+		 * so, made some changes in comparator.
+		 */
+		TreeSet1 set = new TreeSet1((o1,o2) -> {
+			/*
+			 * check whether the selected field, in this case i,
+			 * is equal or not,
+			 * if true : call hashCode() to check equality of the hash codes,
+			 *           if true : call equal() to check equality,
+			 *                     if true : return 0 | meaning they are same
+			 *                     if false : go for next element, in this case j, check their equality
+			 *                                if true : go for next, in this case k, k will not be equal since i and j are same,
+			 *                                			and objects o1 and o2 are not equal.
+			 *                                			return the difference of k.                                
+			 *                                if false : return the difference of j.
+			 *           if false : if false : go for next element, in this case j, check their equality
+			 *                                if true : go for next, in this case k, k will not be equal since i and j are same,
+			 *                                			and objects o1 and o2 are not equal.
+			 *                                			return the difference of k.                                
+			 *                                if false : return the difference of j.           
+			 */
+			if(((C)o1).i == ((C)o2).i) {
+				if(((C)o1).hashCode() == ((C)o2).hashCode()) {
+					if(((C)o1).equals(o2)) return 0;
+					else return ((C)o1).j == ((C)o2).j ? ((C)o1).k - ((C)o2).k : ((C)o1).j - ((C)o2).j ;
+				}
+				else return ((C)o1).j == ((C)o2).j ? ((C)o1).k - ((C)o2).k : ((C)o1).j - ((C)o2).j ;
+			}
+			else return ((C)o1).i - ((C)o2).i;
+		});
+		System.out.println(set.addMany(new C(2,3,4),new C(4,5,6),new C(6,5,4)));
+		System.out.println(set.addMany(new C(3,4,5),new C(4,5,6),new C(4,5,6),new C(6,6,6)));
+		System.out.println(set.addMany(new C(2,2,5),new C(4,5,6),new C(6,5,4),new C(10,9,9)));
 		System.out.println(set);
 	}
-
 }
