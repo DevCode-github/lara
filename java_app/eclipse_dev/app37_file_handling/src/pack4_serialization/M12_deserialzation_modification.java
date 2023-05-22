45f961c1e8588b69d04ff50d9fab8e8fb4241258
Dev <divyanshu.sunnykumar1999@gmail.com>
2023-05-22 10:16:15 +0530

    adding description

--- java_app/eclipse_dev/app37_file_handling/src/pack4_serialization/M12_deserialzation_modification.java
+++ java_app/eclipse_dev/app37_file_handling/src/pack4_serialization/M12_deserialzation_modification.java
@@ -7,6 +7,11 @@ public class M12_deserialzation_modification {
 	public static void main(String[] args) throws Exception{
 		FileInputStream fin = new FileInputStream("test.ser");
 		ObjectInputStream in = new ObjectInputStream(fin);
+		/*
+		 * if there are any modification in the class blueprint after serialization is done,
+		 * then it will throw InvalidclassException, since serialVersionUID was also changed during changes.
+		 * 
+		 */
 		H h1 = (H)in.readObject();
 		System.out.println(h1.i);
 		in.close();
