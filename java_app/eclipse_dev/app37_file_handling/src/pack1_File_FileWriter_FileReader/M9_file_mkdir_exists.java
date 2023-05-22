1b212d0723732ea170a388e35d5cd2b48b029a26
Dev <divyanshu.sunnykumar1999@gmail.com>
2023-05-22 20:10:14 +0530

    removed suppressed warnings statements

--- java_app/eclipse_dev/app37_file_handling/src/pack1_File_FileWriter_FileReader/M9_file_mkdir_exists.java
+++ java_app/eclipse_dev/app37_file_handling/src/pack1_File_FileWriter_FileReader/M9_file_mkdir_exists.java
@@ -3,7 +3,6 @@ package pack1_File_FileWriter_FileReader;
 import java.io.File;
 
 public class M9_file_mkdir_exists {
-	@SuppressWarnings({ "unchecked", "rawtypes", "unused" })
 	public static void main(String[] args) {
 		File f1 = new File("F:\\dir");//does'nt throw error, return false if path does not exist
 		System.err.println(f1.mkdir());
