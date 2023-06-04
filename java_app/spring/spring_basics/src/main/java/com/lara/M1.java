d416bf273cdfd2194bf945a79493301475c4c15e
Dev <divyanshu.sunnykumar1999@gmail.com>
2023-06-04 10:59:35 +0530

    started spring, creating project and configuring beans and pom

---
+++ java_app/spring/spring_basics/src/main/java/com/lara/M1.java
@@ -0,0 +1,18 @@
+package com.lara;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class M1 {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		
+		
+		Person obj = context.getBean("p1", Person.class);
+		System.out.println(obj.getFirstName());
+		System.out.println(obj.getLastName());
+		System.out.println(obj.getAge());
+		Person obj1 = context.getBean("p2", Person.class);
+		System.out.println(obj1.getFirstName());
+		System.out.println(obj1.getLastName());
+		System.out.println(obj1.getAge());
+	}
+}
